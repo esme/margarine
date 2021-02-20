721de0ca5b0db473f7b23170500798125d13510b
Sean Sica <sica@bu.edu>
2021-02-20 14:03:17 -0500

    added mongodb support and successfuly tested using internal in-memory db

--- src/main/java/com/margarine/MargarineApplication.java
+++ src/main/java/com/margarine/MargarineApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class MargarineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MargarineApplication.class, args);
     }
-
 
 }
--- src/main/java/com/margarine/TrackerController.java
+++ src/main/java/com/margarine/TrackerController.java
@@ -4,17 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/bread")
+//@RestController
+//@RequestMapping("/bread")
 public class TrackerController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
     String get(){
         //mapped to hostname:port/bread/
         return "Hello from /bread/";
     }
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    //@RequestMapping(value = "/index", method = RequestMethod.GET)
     String index(){
         //mapped to hostname:port/bread/index/
         return "Hello from /bread/index";
