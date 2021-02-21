8fe1fcea38eb7ffef345958e853e9d9e7d79b91a
Sean Sica <sica@bu.edu>
2021-02-20 20:08:41 -0500

    post method successfully accepting client parameters from API body, but DB store requests are returning DuplicateKeyException

--- src/main/java/com/margarine/db/UrlRepository.java
+++ src/main/java/com/margarine/db/UrlRepository.java
@@ -3,7 +3,7 @@ package com.margarine.db;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "urls", itemResourceRel = "url", path = "urls")
+@RepositoryRestResource(collectionResourceRel = "url", path = "url")
 public interface UrlRepository extends MongoRepository<UrlItem, String> {
     /**
      * The MongoRepository superclass includes all the necessary code for reading and writing our domain class to and
@@ -12,4 +12,5 @@ public interface UrlRepository extends MongoRepository<UrlItem, String> {
      * The @RepositoryRestResource annotation tells Spring Boot to automatically generate a REST endpoint for the data
      * using the HAL JSON specification.
      */
+
 }
