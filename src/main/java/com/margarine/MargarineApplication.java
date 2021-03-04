5406ed65408a3e0f6f0a1dfdcb33c434ae02b8e0
Sean Sica <sica@bu.edu>
2021-03-03 22:50:19 -0500

    changed annotations to resolve autowire issue

--- src/main/java/com/margarine/MargarineApplication.java
+++ src/main/java/com/margarine/MargarineApplication.java
@@ -6,26 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration @EnableAutoConfiguration @ComponentScan
 public class MargarineApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(MargarineApplication.class, args);
     }
-
-    //@Autowired
-    //private UrlRepository urlRepository;
-
-    //@Override
-    //public void run(String... args) throws Exception {
-
-        // Save the employees
-        //urlRepository.save(new UrlItem(1, "google.com", "goo123"));
-
-        // Fetch the all URLs
-        //List<UrlItem> allUrlsInDb = urlRepository.findAll();
-        //System.out.println("============ List of all Stored URLs ============");
-        //System.out.println(allUrlsInDb);
-    //}
-
 }
