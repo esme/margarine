e382c60823a5aefcb49f47a08d863938115dcf0c
esme <esmelingyr@gmail.com>
2021-03-18 13:08:25 -0400

    Add data visualization charts for dashboard - Add react leaflet for graph - Add cards to show visitors - Style css for components

--- src/react-frontend/src/Dashboard.js
+++ src/react-frontend/src/Dashboard.js
@@ -1,59 +1,117 @@
 import { useState } from 'react';
-import TextField from '@material-ui/core/TextField';
 import { DataGrid } from '@material-ui/data-grid';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Graph from './Graph';
+
+const useStyles = makeStyles({
+  root: {
+    '& .grid-theme': {
+      fontFamily: 'Ebrima',
+    },
+    '& .header-theme': {
+      backgroundColor: 'black',
+      color: 'white'
+    },
+    '& .cell-theme': {
+      backgroundColor: 'rgba(255, 255, 255, 0.3)'
+    },
+  },
+});
 
 function Dashboard () {
+  const classes = useStyles();
+
   const [shortUrl, setShortUrl] = useState("");
   const [company, setCompany] = useState("");
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'company', headerName: 'Company', width: 130 },
-    { field: 'url', headerName: 'URL', width: 480 },
+    { 
+      field: 'shortUrl',
+      headerClassName: 'grid-theme header-theme',
+      cellClassName: 'grid-theme cell-theme',
+      headerName: 'Short link',
+      width: 420
+    },
+    { 
+      field: 'originalUrl',
+      headerClassName: 'grid-theme header-theme',
+      cellClassName: 'grid-theme cell-theme',
+      headerName: 'Original Link',
+      width: 420
+    },
     {
-      field: 'clicks',
-      headerName: 'Number of Clicks',
-      type: 'number',
-      width: 180,
+      field: 'dateCreated',
+      headerClassName: 'grid-theme header-theme',
+      cellClassName: 'grid-theme cell-theme',
+      headerName: 'Date Created',
+      type: 'date',
+      width: '100%',
     }
   ];
 
   const rows = [
-    { id: 1, company: 'Snow', url: 'https://en.wikipedia.org/wiki/Jon', clicks: 35 },
-    { id: 2, company: 'Lannister', url: 'https://en.wikipedia.org/wiki/Cersei', clicks: 42 },
-    { id: 3, company: 'Lannister', url: 'https://en.wikipedia.org/wiki/Jaime', clicks: 45 },
-    { id: 4, company: 'Stark', url: 'https://en.wikipedia.org/wiki/Arya', clicks: 16 },
-    { id: 5, company: 'Targaryen', url: 'https://en.wikipedia.org/wiki/Daenerys', clicks: null },
-    { id: 6, company: 'Melisandre', url: null, clicks: 150 },
-    { id: 7, company: 'Clifford', url: 'https://en.wikipedia.org/wiki/Ferrara', clicks: 44 },
-    { id: 8, company: 'Frances', url: 'https://en.wikipedia.org/wiki/Rossini', clicks: 36 },
-    { id: 9, company: 'Roxie', url: 'https://en.wikipedia.org/wiki/Harvey', clicks: 65 },
+    { id: 1, shortUrl: 'https://en.wikipedia.org/wiki/Snow', originalUrl: 'https://en.wikipedia.org/wiki/Jon', dateCreated: Date.now() },
+    { id: 2, shortUrl: 'https://en.wikipedia.org/wiki/Lannister', originalUrl: 'https://en.wikipedia.org/wiki/Cersei', dateCreated: Date.now() },
+    { id: 3, shortUrl: 'https://en.wikipedia.org/wiki/Lannister', originalUrl: 'https://en.wikipedia.org/wiki/Jaime', dateCreated: Date.now() },
+    { id: 4, shortUrl: 'https://en.wikipedia.org/wiki/Stark', originalUrl: 'https://en.wikipedia.org/wiki/Arya', dateCreated: Date.now() },
+    { id: 5, shortUrl: 'https://en.wikipedia.org/wiki/Targaryen', originalUrl: 'https://en.wikipedia.org/wiki/Daenerys', dateCreated: Date.now() },
+    { id: 6, shortUrl: 'https://en.wikipedia.org/wiki/Melisandre', originalUrl: null, dateCreated: Date.now() },
+    { id: 7, shortUrl: 'https://en.wikipedia.org/wiki/Clifford', originalUrl: 'https://en.wikipedia.org/wiki/Ferrara', dateCreated: Date.now() },
+    { id: 8, shortUrl: 'https://en.wikipedia.org/wiki/Frances', originalUrl: 'https://en.wikipedia.org/wiki/Rossini', dateCreated: Date.now() },
+    { id: 9, shortUrl: 'https://en.wikipedia.org/wiki/Roxie', originalUrl: 'https://en.wikipedia.org/wiki/Harvey', dateCreated: Date.now() },
   ];
 
   return (
     <>
       <h1>Dashboard</h1>
-      <TextField
-        id="outlined-basic"
-        label="Enter short URL"
-        variant="outlined"
-        value={shortUrl}
-        type="text"
-        onChange={e => setShortUrl(e.target.value)}
-      ></TextField>
-      <br/>
-      <TextField
-        id="outlined-basic"
-        label="Enter company"
-        variant="outlined"
-        value={company}
-        type="text"
-        onChange={e => setCompany(e.target.value)}
-      ></TextField>
-      <br/>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+      <div className="form">
+        <div>Track your URL:</div>
+        <input
+          className="inputRounded inputText"
+          value={shortUrl}
+          type="text"
+          onChange={e => setShortUrl(e.target.value)}
+        />
+        <br/>
+        <div>Search by Company Name:</div>
+        <input
+          className="inputRounded inputText"
+          value={company}
+          type="text"
+          onChange={e => setCompany(e.target.value)}
+        />
       </div>
+      <div style={{ height: 400, width: '80%', margin: '25px auto 0 auto' }} className={classes.root}>
+        <DataGrid rows={rows} columns={columns} pageSize={5} />
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <div className="container">
+            <h4>Total Unique Visitors</h4>
+            <h1>786</h1>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container">
+            <h4>Date Last Accessed</h4>
+            <h1>2/24/21</h1>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container">
+            <h4>Most Visitors From</h4>
+            <h1>MA</h1>
+            <p>USA</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container">
+            <h4>Visitors Today</h4>
+            <h1>12</h1>
+          </div>
+        </div>
+      </div>
+      <Graph />
     </>
   );
 }
