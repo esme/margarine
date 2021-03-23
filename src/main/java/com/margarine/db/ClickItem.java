becaef20613dce465d3fb11e4725a53907f97a99
Sean Sica <sica@bu.edu>
2021-03-22 23:35:49 -0400

    merged LocationItem data into ClickItem to save space; re-worked unique _id field in ClickItem to maintain a unique id

--- src/main/java/com/margarine/db/ClickItem.java
+++ src/main/java/com/margarine/db/ClickItem.java
@@ -3,23 +3,32 @@ package com.margarine.db;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
-@Document  // marks this class as defining a MongoDB document data model
+//@Document  // marks this class as defining a MongoDB document data model
 @Data  // Lombok helper that save us from the drudgery of creating various getters, setters, and constructors.
 //@AllArgsConstructor  // Lombok helper ^
-@NoArgsConstructor  // Lombok helper ^
+//@NoArgsConstructor  // Lombok helper ^
 public class ClickItem {
 
-    @Id
-    private String id;            // random unique object identifier
-    private LocationItem location;  // location of user who clicked url
-    private Date timeClicked;
+    @Id private String id; // random unique object identifier
+    @Transient private static int counter = 0;
 
-    public ClickItem (String id, LocationItem location, Date timeClicked) {
-        this.id = id;
-        this.location = location;
+    private Date timeClicked;
+    private long longitude;
+    private long latitude;
+
+    public ClickItem (long longitude, long latitude, Date timeClicked) {
+        if (counter == 0){
+            this.id = String.valueOf(counter++);
+        }
+        else{
+            this.id = String.valueOf(++counter);
+        }
+        this.longitude = longitude;
+        this.latitude = latitude;
         this.timeClicked = timeClicked;
     }
 }
