423bb315bfec93d4b6527206ec3f50bbafe80862
Sean Sica <sica@bu.edu>
2021-04-04 22:09:10 -0400

    changed function to only match specific SPA routes

--- src/main/java/com/margarine/server/RouteController.java
+++ src/main/java/com/margarine/server/RouteController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RouteController {
 
-    /** Wildcard route that redirects all root directory GET requests to index.html. Reason is app is an SPA. */
-    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
-    public String redirect() {
+    /** SPA route that redirects all root directory GET requests to index.html. */
+    @RequestMapping(value = {"/", "/home", "index", "index.html", "dashboard", "dashboard.html"}, method = RequestMethod.GET)
+    public String redirectHome() {
         return "forward:/index.html";
     }
+
+    //    /** Wildcard route that redirects all root directory GET requests to index.html. Reason is app is an SPA. */
+    //    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
+    //    public String redirectShortUrlClick() {
+    //        return "forward:/index.html";
+    //    }
 }
