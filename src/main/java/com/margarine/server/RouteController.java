1116b7dc5692f9b54480fbad026f83f928a8af03
Sean Sica <sica@bu.edu>
2021-04-04 21:14:00 -0400

    added javadoc to funtion

--- src/main/java/com/margarine/server/RouteController.java
+++ src/main/java/com/margarine/server/RouteController.java
@@ -2,11 +2,13 @@ package com.margarine.server;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RouteController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    /** Wildcard route that redirects all root directory GET requests to index.html. Reason is app is an SPA. */
+    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
     public String redirect() {
         return "forward:/index.html";
     }
