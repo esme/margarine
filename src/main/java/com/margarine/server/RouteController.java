4e1796236c16e4595062604104367c5396e8ef43
Sean Sica <sica@bu.edu>
2021-04-04 22:15:59 -0400

    specified MediaType consumption model

--- src/main/java/com/margarine/server/RouteController.java
+++ src/main/java/com/margarine/server/RouteController.java
@@ -1,5 +1,6 @@
 package com.margarine.server;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RouteController {
 
     /** SPA route that redirects all root directory GET requests to index.html. */
-    @RequestMapping(value = {"/", "/home", "dashboard", "dashboard.html"}, method = RequestMethod.GET)
-    public String redirectHome() {
-        return "forward:/index.html";
-    }
+    @RequestMapping(
+            value = {"/", "/home", "dashboard", "dashboard.html"},
+            method = RequestMethod.GET,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String redirect() { return "forward:/index.html"; }
 
     //    /** Wildcard route that redirects all root directory GET requests to index.html. Reason is app is an SPA. */
     //    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
