afb38bc6a3c89ce44e29463899669c31f28743f1
Sean Sica <sica@bu.edu>
2021-04-12 17:23:00 -0400

    added fix for running on prod

--- src/react-frontend/src/api.js
+++ src/react-frontend/src/api.js
@@ -1,9 +1,19 @@
 // Read the host address and the port from the environment
+let method = 'http'
 let hostname = 'localhost';
 let port = '8080';
 if (process.env.NODE_ENV === "production") {
   hostname = 'margarine.app';
   port = '8443';
+  method = 'https';
 }
 
-export const REST_API_URL = `http://${hostname}:${port}`;
+let location = window.location.origin; // Returns:'https://margarine.app:8443'
+
+if (location.toString().split(":")[2] === "8443") {
+  hostname = 'margarine.app';
+  port = '8443';
+  method = 'https';
+}
+
+export const REST_API_URL = `${method}://${hostname}:${port}`;
