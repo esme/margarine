74ef3d6b22a1606858437ec5ddfa4b5346986336
esme <esmelingyr@gmail.com>
2021-04-12 12:05:43 -0400

    Set public rest api url for production mode

--- src/react-frontend/src/api.js
+++ src/react-frontend/src/api.js
@@ -1,5 +1,9 @@
 // Read the host address and the port from the environment
-const hostname = process.env.HOST || 'localhost';
-const port = process.env.PORT || '8080';
+let hostname = 'localhost';
+let port = '8080';
+if (process.env.NODE_ENV === "production") {
+  hostname = 'margarine.app';
+  port = 8443;
+}
 
 export const REST_API_URL = `http://${hostname}:${port}`;
